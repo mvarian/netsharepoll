c77ce9e9f2c4896740f1a4571376c3c76d3ad588
mike <mvarian@gmail.com>
2017-02-07 15:29:02 -0600

    First

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # netsharepoll
-A simple shell script to connect to a network share and read a remote file, useful for generating baseline data on the performance of the network share for troubleshooting purposes.
+A simple shell script to connect to a network share and read a remote file at a specified interval, useful for generating baseline data on the performance of the network share for troubleshooting purposes.
+
+
+
+## Features
+
+* Designed to be deployed into a client environment and gather basic performance metrics by reading from one or more network shares.  
+* Will connect to a network share at the configured interval, navigate to a directory, and read the contents of a specified file.
+* Supports accepting an array of multiple remote file locations which are read in a round robin format on each iteration of the script.
+* Data is appended to a log file in csv format with the network share, remote file, timestamp, and duration in ms of the operation.  
+* If the remote read fails, "FAIL" will be inserted for the duration value.
+* If the remote file does not exist, "MISSING" will be inserted for the duration value.
+* If the remote connection cannot be established, "CONNECT FAIL" will be inserted for the duration value.
+* Script will make sure that only one instance is running at one time, if it is executed multiple times the subsequent executions will abort.
+* For unattended execution, schedule the script to run every minute in cron.  The script will automatically begin on boot, and if it crashes it will resume within 60 seconds.
+* To fetch log files remotely, consider scheduled mailx, mutt, or scp to a remote server.  Alternatively, set up a basic samba share and fetch from another machine on the network.
+* Currently supports smb only (Windows shares)
+
+
+## Usage
+
+1. In `netsharepoll.sh`, configure NETSHARE, USERNAME, PASSWORD, REMOTEFILES, and INTERVAL.
+
+2. Run `netsharepoll.sh` in a terminal session, or schedule it to run in cron.
+
+3. `netsharepoll.sh` will continue to run until killed.  Output is written in csv format in LOGPATH and can be opened by any spreadsheet program.
+
+To gather data on a second server simultaneously, make a copy of `netsharepoll.sh`, change the NETSHARE, USERNAME, PASSWORD, and REMOTEFILES variables so they are different, and run as normal.
+
+
+## Supported Platforms
+
+netsharepoll has only been tested on CentOS 7.  It should also work on any other Redhat-compatible Linux distribution.
---
+++ netsharepoll.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# netsharepoll.sh
+# https://github.com/mvarian/netsharepoll
+# MIT License
+
+# Configure NETSHARE for your situation
+NETSHARE="\\\\WINDOWS-SERVER\SHARE-NAME"
+USERNAME="USER"
+PASSWORD="PASSWORD"
+
+# Configure the remote files, these files must exist on the network share
+REMOTEFILES=('\folder\remotefile1.txt' '\folder\remotefile2.txt' '\folder\remotefile3.txt')
+
+# Set ping interval in seconds
+INVERVAL=60
+
+# Set destination log file
+LOGPATH="$NETSHARE-netshare_poll.log.csv"
+
+# Used to track execution of this unique script to ensure only one instance is running at a time
+PIDFILE="$NETSHARE-netsharepoll.sh.pid"
+
+
+#================ Code below is not meant to be modified ================#
+
+if [ -f $PIDFILE ] 
+then
+	echo "[$(date)] : netsharepoll.sh : Script is already running for $NETSHARE"
+	exit 1
+fi
+
+# Ensure PID file is removed on program exit.
+trap "rm -f -- '$PIDFILE'" EXIT
+
+# Create a file with current PID to indicate that process is running.
+echo $$ > "$PIDFILE"
+
+
+# If log file does not yet exist, initialize it with column headers
+if [ ! -f $LOGPATH ] 
+then
+	echo '"Network Share","Remote File","Timestamp","Duration"' > $LOGPATH
+fi
+
+# Array counters
+ARRAYSIZE=${#REMOTEFILES[*]}
+ARRAYINDEX=0
+
+# Infinite loop to record the latency of a ping every INTERVAL until script is aborted
+while [ true ] 
+do
+
+	RFILE=${REMOTEFILES[$ARRAYINDEX]}
+
+	# Start timer
+	STIME=$(date +%s.%N)
+
+	# Execute network share remote file read
+	RESULT=`smbclient "$NETSHARE" -U "$USERNAME%$PASSWORD" -c "get $RFILE"`
+
+	# End timer
+	ETIME=$(date +%s.%N)
+	
+	# Wrap-up
+	DURATION=`echo - | awk "{print $ETIME - $STIME}"`
+
+	if [[ "$RESULT" == *"NT_STATUS_BAD_NETWORK_NAME"* ]] 
+	then
+		DURATION="CONNECT FAIL"
+	fi
+
+	if [[ "$RESULT" == *"NT_STATUS_OBJECT_NAME_NOT_FOUND"* ]] 
+	then
+		DURATION="MISSING"
+	fi
+
+	if [[ "$RESULT" == *"NT_STATUS"* ]] 
+	then
+		DURATION="FAIL"
+	fi
+
+	if [ -z "$DURATION" ] 
+	then
+		DURATION="FAIL"
+	fi
+
+	if [ -f "$RFILE" ] 
+	then
+		rm $RFILE
+	fi
+
+	# Log results
+	NOW=`date +'%F %H:%M:%S'`
+	echo "\"$NETSHARE\",\"$RFILE\",\"$NOW\",\"$DURATION\"" >> "$LOGPATH"
+	
+	# Increment current index
+	((ARRAYINDEX=ARRAYINDEX+1))
+
+	# Reset index to 0 if we've made it all the way around to prevent integer overflow on counter
+	if [ $((ARRAYINDEX)) -eq $((ARRAYSIZE)) ] 
+	then
+		ARRAYINDEX=0
+	fi
+
+	# Pause until next interval
+	sleep "$INVERVAL"
+
+done
